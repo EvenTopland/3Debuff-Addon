82ad97babd24ad60d5c63da21db04e40560fa378
EvenTopland <54711928+EvenTopland@users.noreply.github.com>
2024-07-05 22:16:58 +0900

    Changed to blender's timing system

--- 3Debuff.py
+++ 3Debuff.py
@@ -6,36 +6,36 @@ bl_info = {
 
 import bpy
 import random
-import threading
 
 def move_objects():
     for obj in bpy.data.objects:
         obj.location.x += random.uniform(-3, 3)
         obj.location.y += random.uniform(-3, 3)
         obj.location.z += random.uniform(-3, 3)
+    return 120  # Run every 2 minutes
 
 def delete_random_object():
     if bpy.data.objects:
         obj = random.choice(bpy.data.objects)
         bpy.data.objects.remove(obj)
+    return 300  # Run every 5 minutes
 
 def rotate_objects():
     for obj in bpy.data.objects:
         obj.rotation_euler.x += random.uniform(-0.4, 0.4)
         obj.rotation_euler.y += random.uniform(-0.4, 0.4)
         obj.rotation_euler.z += random.uniform(-0.4, 0.4)
+    return 120  # Run every 2 minutes
 
 def shutdown_blender():
-    # Generate a random delay between 1 to 5 minutes (60 to 300 seconds)
     delay_seconds = random.randint(60, 300)
-    # Start a timer to shut down Blender after the random delay
-    threading.Timer(delay_seconds, bpy.ops.wm.quit_blender).start()
-    
+    bpy.app.timers.register(bpy.ops.wm.quit_blender, first_interval=delay_seconds)
+
 def change_light_colors():
     for obj in bpy.data.objects:
         if obj.type == 'LIGHT':
             obj.data.color = (random.random(), random.random(), random.random())
-            threading.Timer(300, change_light_colors).start()
+    return 300  # Run every 5 minutes
 
 def add_random_modifier():
     modifiers = [
@@ -43,19 +43,13 @@ def add_random_modifier():
         "SUBSURF", "WIREFRAME", "DISPLACE", "SIMPLE_DEFORM", "WAVE", "OCEAN"
     ]
     
-    # Get all mesh objects
     mesh_objects = [obj for obj in bpy.data.objects if obj.type == 'MESH']
     
     if mesh_objects:
-        # Choose a random mesh object
         obj = random.choice(mesh_objects)
-        
-        # Choose a random modifier
         mod_type = random.choice(modifiers)
-        
-        # Add the modifier to the chosen object
         obj.modifiers.new(name=mod_type, type=mod_type)
-
+    return 300  # Run every 5 minutes
 
 def disable_undo():
     keyconfig = bpy.context.window_manager.keyconfigs.active
@@ -67,40 +61,17 @@ def disable_undo():
 def disable_delete_button():
     keyconfig = bpy.context.window_manager.keyconfigs.active
     
-    # Disable in 'Object Mode'
-    keymap_object_mode = keyconfig.keymaps.get('Object Mode')
-    if keymap_object_mode:
-        for kmi in keymap_object_mode.keymap_items:
-            if kmi.type in {'X', 'DEL'}:
-                keymap_object_mode.keymap_items.remove(kmi)
-    
-    # Disable in 'Edit Mode'
-    keymap_edit_mode = keyconfig.keymaps.get('Mesh')
-    if keymap_edit_mode:
-        for kmi in keymap_edit_mode.keymap_items:
-            if kmi.type in {'X', 'DEL'}:
-                keymap_edit_mode.keymap_items.remove(kmi)
+    for keymap_name in ['Object Mode', 'Mesh']:
+        keymap = keyconfig.keymaps.get(keymap_name)
+        if keymap:
+            for kmi in keymap.keymap_items:
+                if kmi.type in {'X', 'DEL'}:
+                    keymap.keymap_items.remove(kmi)
 
-def move_objects_periodic():
-    move_objects()
-    threading.Timer(120, move_objects_periodic).start()
-
-def delete_random_object_periodic():
-    delete_random_object()
-    threading.Timer(300, delete_random_object_periodic).start()
-
-def rotate_objects_periodic():
-    rotate_objects()
-    threading.Timer(120, rotate_objects_periodic).start()
-
-def add_random_modifier_periodic():
-    add_random_modifier()
-    threading.Timer(300, add_random_modifier_periodic).start()
-    
 ui_scale = 0.5
 scale_direction = 1
 
-def change_ui_scale_periodic():
+def change_ui_scale():
     global ui_scale, scale_direction
     ui_scale += 0.1 * scale_direction
     if ui_scale >= 2.0:
@@ -108,8 +79,7 @@ def change_ui_scale_periodic():
     elif ui_scale <= 0.5:
         scale_direction = 1
     bpy.context.preferences.view.ui_scale = ui_scale
-    threading.Timer(5, change_ui_scale_periodic).start()
-
+    return 5  # Run every 5 seconds
 
 class OBJECT_OT_move_objects(bpy.types.Operator):
     bl_idname = "object.move_objects"
@@ -117,7 +87,7 @@ class OBJECT_OT_move_objects(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        move_objects_periodic()
+        bpy.app.timers.register(move_objects)
         return {'FINISHED'}
 
 class OBJECT_OT_delete_random_object(bpy.types.Operator):
@@ -126,7 +96,7 @@ class OBJECT_OT_delete_random_object(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        delete_random_object_periodic()
+        bpy.app.timers.register(delete_random_object)
         return {'FINISHED'}
 
 class OBJECT_OT_rotate_objects(bpy.types.Operator):
@@ -135,7 +105,7 @@ class OBJECT_OT_rotate_objects(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        rotate_objects_periodic()
+        bpy.app.timers.register(rotate_objects)
         return {'FINISHED'}
 
 class OBJECT_OT_shutdown_blender(bpy.types.Operator):
@@ -146,16 +116,15 @@ class OBJECT_OT_shutdown_blender(bpy.types.Operator):
     def execute(self, context):
         shutdown_blender()
         return {'FINISHED'}
-    
+
 class OBJECT_OT_random_light_color(bpy.types.Operator):
     bl_idname = "object.random_light_color"
     bl_label = "Randomize Light Colors"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        change_light_colors()
+        bpy.app.timers.register(change_light_colors)
         return {'FINISHED'}
-    
 
 class OBJECT_OT_add_random_modifier(bpy.types.Operator):
     bl_idname = "object.add_random_modifier"
@@ -163,17 +132,7 @@ class OBJECT_OT_add_random_modifier(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        add_random_modifier_periodic()
-        return {'FINISHED'}
-
-
-class OBJECT_OT_randomize_inputs(bpy.types.Operator):
-    bl_idname = "object.randomize_inputs"
-    bl_label = "Randomize Inputs"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        randomize_inputs()
+        bpy.app.timers.register(add_random_modifier)
         return {'FINISHED'}
 
 class OBJECT_OT_disable_undo(bpy.types.Operator):
@@ -193,14 +152,14 @@ class OBJECT_OT_disable_delete_button(bpy.types.Operator):
     def execute(self, context):
         disable_delete_button()
         return {'FINISHED'}
-    
+
 class OBJECT_OT_change_scale(bpy.types.Operator):
     bl_idname = "ui.change_scale"
     bl_label = "Change UI Scale Periodically"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        change_ui_scale_periodic()
+        bpy.app.timers.register(change_ui_scale)
         return {'FINISHED'}
 
 class VIEW3D_PT_chaos_panel(bpy.types.Panel):
@@ -233,7 +192,6 @@ def register():
     bpy.utils.register_class(OBJECT_OT_change_scale)
     bpy.utils.register_class(OBJECT_OT_random_light_color)
     bpy.utils.register_class(VIEW3D_PT_chaos_panel)
-
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_move_objects)
